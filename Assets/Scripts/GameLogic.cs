66c063627084f0b919d34ac4a883710f6eb4666e
李一帆 <liyifan@liyifandeMacBook-Pro.local>
2019-10-06 18:30:29 +0100

    Initial commit

---
+++ Assets/Scripts/GameLogic.cs
@@ -0,0 +1,57 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class GameLogic : MonoBehaviour
+{
+    [SerializeField] int max;
+    [SerializeField] int min;
+    [SerializeField] TextMeshProUGUI guessText;
+    [SerializeField] TextMeshProUGUI trickText;
+    [SerializeField] GameObject higherButton;
+    [SerializeField] GameObject lowerButton;
+
+    int guess;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        StartGame();
+    }
+
+    void StartGame()
+    {
+        NextGuess();
+        trickText.GetComponent<CanvasGroup>().alpha = 0f;
+        max += 1;
+    }
+
+    public void onPressHigher()
+    {
+        min = guess + 1;
+        NextGuess();
+    }
+
+    public void onPressLower()
+    {
+        max = guess;
+        NextGuess();
+    }
+
+    public void NextGuess()
+    {
+        if (min == max)
+        {
+            trickText.GetComponent<CanvasGroup>().alpha = 1f;
+            higherButton.SetActive(false);
+            lowerButton.SetActive(false);
+
+        }
+        else { 
+            guess = Random.Range(min, max);
+            guessText.text = guess.ToString();
+            //Debug.Log("min: " + min + ", max: " + max);
+        }
+    }
+}
